0326b8ad759e6f616398c949e542b60c05a6a025
Gouki <ares123@hotmail.com>
2021-04-10 11:56:40 -0400

    subiendo punto 1 y 3

---
+++ CsvApp/api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from CsvApp.models import MasterProductsConfigurable
+
+class MasterProductsConfigurableSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MasterProductsConfigurable
+        fields = ['model','name','price']
+
--- dockerDjangoCsv/settings.py
+++ dockerDjangoCsv/settings.py
@@ -30,15 +30,25 @@ ALLOWED_HOSTS = ['*']
 
 # Application definition
 
-INSTALLED_APPS = [
+BASE_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
+]
+
+LOCAL_APPS = [    
     'CsvApp',
 ]
+
+THIRD_APPS = [
+    
+]
+
+INSTALLED_APPS = BASE_APPS + LOCAL_APPS + THIRD_APPS
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
